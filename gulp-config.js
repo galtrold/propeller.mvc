ccd9c4b65324c57b4db5c91509c36ceac0fdc292
cjh <cjh@pentia.dk>
2016-10-18 21:20:58 +0200

    initial commit

---
+++ gulp-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    websitePath: "C:\\Websites\\propeller\\Website",
+    solutionPath: "f:\\Home\\James\\projects\\propeller.mvc",
+    buildConfiguration: "Debug"
+}
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+/// <binding />
+var gulp = require("gulp");
+var config = require("./gulp-config.js"); 
+var msbuild = require("gulp-msbuild");
+var foreach = require("gulp-foreach");
+var debug = require("gulp-debug");
+var path = require("path");
+var args = require('yargs').argv;
+
+gulp.task('push', function() {
+   
+   publishProjects(config.solutionPath + "\\src", config.websitePath)
+   
+});
+
+var publishProjects = function (location, dest) {
+  var targets = ["Build"];
+
+  console.log("publish to " + dest + " folder");
+  return gulp.src([location + "/**/*.csproj"])
+    .pipe(foreach(function (stream, file) {
+      return stream
+        .pipe(debug({ title: "Building project:" }))
+        .pipe(msbuild({
+          targets: targets,
+          configuration: config.buildConfiguration,
+          logCommand: false,
+          verbosity: "minimal",
+          stdout: true,
+          errorOnFail: true,
+          maxcpucount: 0,
+          toolsVersion: 14.0,
+          properties: {
+            DeployOnBuild: "true",
+            DeployDefaultTarget: "WebPublish",
+            WebPublishMethod: "FileSystem",
+            DeleteExistingFiles: "false",
+            publishUrl: dest,//"c:\\Websites\\test\\website",
+            _FindDependencies: "false"
+          }
+        }));
+    }));
+};
