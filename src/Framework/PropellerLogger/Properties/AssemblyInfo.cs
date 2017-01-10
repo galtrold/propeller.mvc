1c7f116e41113be7eaade499badcbc9f9b111556
Christia James <james@galtrold.dk>
2017-01-10 22:09:37 +0100

    renamed log projects

--- src/Framework/PropellerLogger/Properties/AssemblyInfo.cs
+++ src/Framework/PropellerLogger/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PropellerTcpLogger")]
+[assembly: AssemblyTitle("PropellerLogger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PropellerTcpLogger")]
+[assembly: AssemblyProduct("PropellerLogger")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
