994d667807f47b69c660305bc23c8446c47f38d0
Christia James <cjh@pentia.dk>
2017-01-13 17:00:33 +0100

    add nuspec file and moved docker folder

--- src/Domain/Demo/Controllers/CharacterController.cs
+++ src/Domain/Demo/Controllers/CharacterController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Web.Mvc;
 using Newtonsoft.Json;
-using Propeller.Mvc.Core.Processing;
 using Propeller.Mvc.Demo.ViewModels;
 using Sitecore.Mvc.Presentation;
 
@@ -11,11 +10,6 @@ namespace Propeller.Mvc.Demo.Controllers
 
         public ActionResult Index()
         {
-            Sitecore.Diagnostics.Log.Info("Pass on that shit!!", this);
-            var mappingProcessor = new MappingProcessor();
-            mappingProcessor.Process(null);
-
-
             var characterViewModel = new CharacterViewModel(RenderingContext.Current.Rendering);
             ViewBag.SerializedModel = JsonConvert.SerializeObject(characterViewModel, Formatting.Indented);
             return View(characterViewModel);
