4b88e65d14c1154b90739a56b6d8c474c1c4c6c9
Christia James <james@galtrold.dk>
2017-08-24 00:02:41 +0200

    change yo yarn

--- src/Framework/PropellerLogger/dist/app/container_startup_script.sh
+++ src/Framework/PropellerLogger/dist/app/container_startup_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Copy propellerlogger binary and configuration to website folder.
+mkdir -p /var/www/App_Config/Include/Propeller.Logger
 cp /app/lib/propeller.logger.dll /var/www/bin/propeller.logger.dll
 cp /app/config/propellerlog.config /var/www/App_Config/Include/Propeller.Logger/propellerlog.config
 
@@ -8,8 +9,8 @@ if [ $doMerge = "true" ]
 then
 	echo "Propeller logger configured against Sitecore 8.0 or lower."
 	echo "Merging UDP appender into Web.config"
-	npm install xpath
-	npm install xmldom
+	yarn global add xpath
+	yarn global add xmldom
 	nodejs /app/config_merge.js
 fi
 
