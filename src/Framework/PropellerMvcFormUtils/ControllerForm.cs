bd5da097c0aac544fff852a52d3d157bb7b6869c
Christia James <cjh@pentia.dk>
2018-04-09 15:31:16 +0200

    Add form utils

---
+++ src/Framework/PropellerMvcFormUtils/ControllerForm.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Propeller.Mvc.FormUtils
+{
+    public static class ControllerForm
+    {
+        public static string ControllerUrl(this HtmlHelper helper)
+        {
+            return helper.ViewContext.HttpContext.Request.RawUrl;
+        }
+
+
+        public static HtmlString ControllerHandler(this HtmlHelper helper, string controllerName = null, string action = "Index")
+        {
+            controllerName = controllerName ?? helper.ViewContext.HttpContext.Request.RawUrl.Trim('/');
+
+            var htmlStr =
+                $"<input type=\"hidden\" name=\"fhController\" value=\"{controllerName}Controller\" />" +
+                $"<input type=\"hidden\" name=\"fhAction\" value=\"{action}\" />";
+            return new HtmlString(htmlStr);
+        }
+    }
+}
