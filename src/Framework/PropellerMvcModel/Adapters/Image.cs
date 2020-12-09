5cdd7806d0de6049e26c9276c6311dea36b98595
Christia James <cjh@pentia.dk>
2020-12-09 17:00:37 +0100

    Fix problem with image adapter when an item has empty standard values.

--- src/Framework/PropellerMvcModel/Adapters/Image.cs
+++ src/Framework/PropellerMvcModel/Adapters/Image.cs
@@ -1,4 +1,5 @@
-﻿using Sitecore.Data;
+﻿using Sitecore;
+using Sitecore.Data;
 using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
 
@@ -11,9 +12,13 @@ namespace Propeller.Mvc.Model.Adapters
 
         public void InitAdapter(Item item, ID propId)
         {
+            Url = string.Empty;
+            Alt = string.Empty;
+
             ImageField image = item.Fields[propId];
-            if (image == null)
+            if (image == null ||image.MediaID == ItemIDs.Null)
                 return;
+            
             var mediaItem = image.MediaDatabase.GetItem(image.MediaID);
             Url = Sitecore.Resources.Media.MediaManager.GetMediaUrl(mediaItem);
             Alt = image.Alt;
